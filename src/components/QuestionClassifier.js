2b6e89f0fdb59cdad4a6881dfe4de19b4423bdc3
zixuan118 <zixuanc1618@gmail.com>
2024-09-30 00:44:45 -0400

    Updates

--- src/components/QuestionClassifier.js
+++
@@ -1,5 +0,0 @@
-export const isZixuanRelated = (question) => {
-    const zixuanKeywords = ["子炫", "子炫的", "zixuan", "Zixuan"];
-    
-    return zixuanKeywords.some(keyword => question.includes(keyword));
-};
